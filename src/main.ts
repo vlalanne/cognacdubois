38f4c7278e3a6a47cfb3af5f45113559285799a3
Vincent LALANNE <vlalanne90@gmail.com>
2022-12-17 14:25:08 +0100

    feat(all) update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,6 @@
-import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { CognacduboisModule } from './app/cognacdubois.module';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
 
 platformBrowserDynamic().bootstrapModule(CognacduboisModule)
   .catch(err => console.error(err));
